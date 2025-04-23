38ec183cb484dcb899bacf2fa47a27da7cba655f
Gh05d <pascla@mailbox.org>
2025-04-23 12:55:51 +0200

    Lots of styling for index page and added 404 page

--- assets/index.mjs
+++ assets/index.mjs
@@ -1,20 +1,24 @@
-(async function fetchPosts() {
-  const res = await fetch("assets/search.json");
-  const posts = await res.json();
-  renderPosts(posts);
-  setupSearch(posts);
-})();
-
 function renderPosts(posts) {
-  const container = document.getElementById("posts");
+  const container = document.getElementById("latest");
   container.innerHTML = "";
   posts.forEach((post) => {
     const div = document.createElement("div");
-    div.className = "post";
+    //     div.className = "post";
+    //     div.innerHTML = `
+    //      <a class="post-title" href="${post.url}">${post.title}</a>
+    //      <div class="post-excerpt">${post.excerpt}</div>
+    //    `;
+    div.className = "card";
     div.innerHTML = `
-     <a class="post-title" href="${post.url}">${post.title}</a>
-     <div class="post-excerpt">${post.excerpt}</div>
-   `;
+    <h3>
+    <a
+      href="/articles/${post.url}"
+      >${post.title}</a
+    </a>
+  </h3>
+
+  <p>${post.excerpt}</p>
+`;
     container.appendChild(div);
   });
 }
@@ -31,7 +35,7 @@ function setupSearch(posts) {
   });
 }
 
-(async function fetchPosts() {
+(async function init() {
   const res = await fetch("assets/search.json");
   const posts = await res.json();
   renderPosts(posts);
